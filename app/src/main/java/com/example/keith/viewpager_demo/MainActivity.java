c794e410df23c26b61f03117136d581d2bd579e8
keith <HotButteredToast1>
2020-03-06 01:09:16 -0500

    Updated gradle

--- app/src/main/java/com/example/keith/viewpager_demo/MainActivity.java
+++ app/src/main/java/com/example/keith/viewpager_demo/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.keith.viewpager_demo;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     ViewPager vp;
     CusomSwipeAdapter csa;
 
